f63e5058df678485246cca36c6fb7b3bbf4bbdcd
 chene2603-cmd  <chene2603@gmail.com>
2026-04-05 18:34:08 +0800

    Create quantum/quantum_bridge.cpp

---
+++ quantum/quantum_bridge.cpp
@@ -0,0 +1,6 @@
+#include "include/DataEncoder.h"
+#include "include/VariationalAnsatz.h"
+#include "include/QuantumDistanceCalculator.h"
+#include "include/ErrorMitigationWrapper.h"
+
+// 预留用于 pybind11 绑定
